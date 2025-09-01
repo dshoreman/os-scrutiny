ec1dc7407273a55f064e50d01ccc1e69321d1dc3
Dave Shoreman <dshoreman@users.noreply.github.com>
2025-09-01 14:50:32 +0100

    Fix page heading in controller

--- src/opnsense/mvc/app/controllers/OPNsense/Scrutiny/SettingsController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Scrutiny/SettingsController.php
@@ -6,7 +6,7 @@ class SettingsController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
-        $this->view->title = 'Scrutiny: Settings';
+        $this->view->title = 'Services: Scrutiny: Settings';
         $this->view->description = 'Configure how the Scrutiny collector runs and logs data.';
 
         $this->view->form = $this->getForm('settings');
